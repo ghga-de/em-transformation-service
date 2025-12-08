6921fa2ef4a6f16bd27dccb8d514c7bd2c2670ef
Christoph Zwerschke <cito@online.de>
2025-12-08 07:50:37 +0000

    Add utility function to determine topological order

---
+++ src/ets/core/graph.py
@@ -0,0 +1,125 @@
+"""Directed graph validation and determination of topological order.
+
+Note that the graphlib module of the standard library already provides a
+similar functionality, validating that the graph does not have cycles, but
+it does not check for the stronger property of unique paths between nodes.
+"""
+
+from collections import defaultdict
+from collections.abc import Hashable, Mapping, Sequence
+from typing import TypeVar
+
+__all__ = [
+    "CyclicGraphError",
+    "InvalidGraphError",
+    "NonUniquePathError",
+    "get_topological_order",
+]
+
+
+# Nodes can be any hashable type
+N = TypeVar("N", bound=Hashable)
+
+
+class InvalidGraphError(ValueError):
+    """Error indicating that the graph is invalid in some way."""
+
+
+class NonUniquePathError(InvalidGraphError):
+    """Error indicating that a non-unique path was detected in the graph."""
+
+
+class CyclicGraphError(NonUniquePathError):
+    """Error indicating that a cycle was detected in the graph."""
+
+
+def get_topological_order(edges: list[tuple[N, N]]) -> dict[N, int]:
+    """Validate a directed graph and get its topological order.
+
+    The graph must be specified as a list of directed edges
+    that are tuples of any hashable objects (the nodes).
+    Isolated nodes are not covered by this function,
+    since they can be ordered arbitrarily anyway.
+
+    The function checks that that any path connecting two nodes
+    is always unique, which implies that the graph is acyclic,
+    but is a stronger property (allowing no diamond shapes).
+
+    Raises CyclicGraphError if a cycle is detected.
+    Raises NonUniquePathError if a non-unique path is detected.
+
+    Returns a mapping of each node to its topological order index.
+    """
+    # extract nodes and build adjacency list and in-degrees
+    nodes = _get_nodes_from_edges(edges)
+    adj = _get_adjacency_list(edges)
+    in_degrees = _get_in_degrees(nodes, adj)
+
+    # perform Kahn's algorithm with unique path check
+    result = _kahn_algorithm(in_degrees, adj)
+
+    # check if there are still edges left
+    if any(in_degrees.values()):
+        raise CyclicGraphError("A cycle was detected in the graph.")
+
+    # return the topological order as a mapping
+    return {node: i for i, node in enumerate(result)}
+
+
+def _get_nodes_from_edges(edges: list[tuple[N, N]]) -> tuple[N, ...]:
+    """Get all nodes from the list of edges."""
+    # use a dict to deduplicate while preserving order
+    nodes: dict[N, None] = {}
+    for from_node, next_node in edges:
+        nodes[from_node] = None
+        nodes[next_node] = None
+    return tuple(nodes)
+
+
+def _get_adjacency_list(edges: list[tuple[N, N]]) -> Mapping[N, list[N]]:
+    """Get the adjacency list from the list of edges."""
+    adj: Mapping[N, list[N]] = defaultdict(list)
+    for from_node, next_node in edges:
+        adj[from_node].append(next_node)
+    return adj
+
+
+def _get_in_degrees(nodes: Sequence[N], adj: Mapping[N, list[N]]) -> dict[N, int]:
+    """Get the in-degrees of all nodes in the graph."""
+    in_degrees: dict[N, int] = defaultdict(int)
+    for current_node in nodes:
+        for next_node in adj[current_node]:
+            in_degrees[next_node] += 1
+    return in_degrees
+
+
+def _kahn_algorithm(in_degrees: dict[N, int], adj: Mapping[N, list[N]]) -> list[N]:
+    """Run Kahn's algorithm for topological ordering with unique path check.
+
+    This is a modified version of Kahn's algorithm that checks the unique path property
+    of the graph along the way and throws an error if it is violated.
+    """
+    ancestors: dict[N, set[N]] = {
+        node: set() if in_degrees[node] else {node} for node in adj
+    }
+
+    result: list[N] = []
+    queue = [node for node in adj if not in_degrees[node]]
+    pop = queue.pop
+    push = queue.append
+    append = result.append
+
+    while queue:
+        current_node = pop()
+        ancestors_of_current = ancestors[current_node]
+        append(current_node)
+        for next_node in adj[current_node]:
+            ancestors_of_next = ancestors[next_node]
+            in_degrees[next_node] -= 1
+            if ancestors_of_next.intersection(ancestors_of_current):
+                raise NonUniquePathError(f"Multiple paths lead to node {next_node}")
+            ancestors_of_next.update(ancestors_of_current)
+            if not in_degrees[next_node]:
+                push(next_node)
+
+    return result
---
+++ tests/test_graph.py
@@ -0,0 +1,100 @@
+"""Test directed graph validation and topological order."""
+
+import pytest
+
+from ets.core.graph import CyclicGraphError, NonUniquePathError, get_topological_order
+
+# For the test cases, graphs are represented as strings,
+# where the edges are given as pairs of letters (from, to).
+
+CYCLIC_GRAPHS = [
+    "AB BA",
+    "AB BC CA",
+    "AB BC CB",
+    "AB BC CD DA",
+    "AB BC CD CB",
+    "AB BC CD DB",
+    "AB BC CD CE FG GH HJ HI IF",
+]
+
+ACYCLIC_GRAPHS_WITH_UNIQUE_PATHS = [
+    "AB",
+    "AC BC",
+    "BD CD",
+    "AC BC DF EF",
+    "AB BC DC DE",
+    "AB CD EF",
+    "AB BC CD EF FG GD DH HI HJ IK JL",
+]
+
+ACYCLIC_GRAPHS_WITH_MULTIPLE_PATHS = [
+    "AB BC AC",
+    "AB AC BD BC",
+    "AB BC BD CE DE",
+    "AB AC BD BE CE CF",
+    "AB AC BD CE CF EG FG",
+    "AB BC CD EF FG FB FC",
+    "AB BC CD EF FB FC",
+    "AD BD BE CE CH DF DF DH",
+    "AB CD CE DF EF FG",
+    "AB BC CD EF FG GD DH HI HJ IK JL KM ML MN",
+]
+
+
+def edges_from_string(s: str) -> list[tuple[str, str]]:
+    """Get a list of edges from a string representation."""
+    return [(s[0], s[1]) for s in s.split()]
+
+
+def assert_is_topological_order(
+    edges: list[tuple[str, str]], order: dict[str, int]
+) -> None:
+    """Assert that the specified order is a valid topological order."""
+    # Check that (only) isolated nodes have order zero
+    for i, node in enumerate(order):
+        assert order[node] == i
+    for edge in edges:
+        from_node, to_node = edge
+        assert from_node in order
+        assert to_node in order
+        assert order[from_node] < order[to_node]
+
+
+def test_validates_simple_graph():
+    """Test that the order of a simple valid graph is returned correctly."""
+    edges = [("A", "B"), ("B", "C")]
+    assert get_topological_order(edges) == {"A": 0, "B": 1, "C": 2}
+
+
+def test_detects_duplicate_edges():
+    """Test that duplicate edges are detected."""
+    edges = [("A", "B"), ("A", "B")]
+    with pytest.raises(NonUniquePathError):
+        get_topological_order(edges)
+
+
+@pytest.mark.parametrize("edge_string", CYCLIC_GRAPHS)
+def test_cyclic_graph(edge_string: str):
+    """Test that cyclic graphs are detected."""
+    edges = edges_from_string(edge_string)
+    with pytest.raises(CyclicGraphError):
+        get_topological_order(edges)
+
+
+@pytest.mark.parametrize("edge_string", ACYCLIC_GRAPHS_WITH_UNIQUE_PATHS)
+def test_acyclic_graph_with_unique_paths(edge_string: str):
+    """Test that acyclic graphs with unique path property are validated properly.
+
+    Also tests that the returned topological order is correct.
+    """
+    edges = edges_from_string(edge_string)
+    order = get_topological_order(edges)
+    assert_is_topological_order(edges, order)
+
+
+@pytest.mark.parametrize("edge_string", ACYCLIC_GRAPHS_WITH_MULTIPLE_PATHS)
+def test_acyclic_graph_with_multiple_paths(edge_string: str):
+    """Test that acyclic graphs without unique path property are detected."""
+    edges = edges_from_string(edge_string)
+    with pytest.raises(NonUniquePathError):
+        get_topological_order(edges)
