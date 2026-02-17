649a35eb9d07c92f4e46a2186ff04827efdd3156
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2026-02-17 13:32:58 +0000

    Short circuit comparison on schema consistency issues + more logging

--- src/ets/core/config_manager.py
+++ src/ets/core/config_manager.py
@@ -50,6 +50,7 @@ class ConfigManager(ConfigManagerPort):
         route_dao: RouteDao,
         workflow_dao: WorkflowDao,
     ):
+        self.use_persisted_config = False
         self.config_path = config_path
         self.model_dao = model_dao
         self.route_dao = route_dao
@@ -65,13 +66,17 @@ class ConfigManager(ConfigManagerPort):
         """
         try:
             await self._compare_configs()
-        except ComparisonMismatchError:
+        except ComparisonMismatchError as error:
+            log.info(
+                f"Changes detected between configs, using new config.\nDetails:{error}"
+            )
             return ComparisonResultChanged(
                 models=self.config_fields.new_models,
                 routes=self.config_fields.routes,
                 workflows=self.config_fields.workflows,
             )
 
+        log.info("No changes detected between configs, continuing with old config.")
         return ComparisonResultUnchanged(
             models=self.config_fields.old_models,
             routes=self.config_fields.routes,
@@ -91,46 +96,26 @@ class ConfigManager(ConfigManagerPort):
             workflows=new_workflows,
         )
 
-        _compare_models(new_models, old_models)
+        # Short circuit if SchemaPack parsing failed and just use old config in that case
+        if self.use_persisted_config:
+            return
+
+        log.info("Comparing models.")
+        try:
+            _compare_models(new_models, old_models)
+        except ValueError as error:
+            # Short circuit on invalid is_ingress/schema_ combinations and use the old config
+            log.error(error)
+            log.info("Falling back to using old config.")
+            return
+        log.info("Comparing routes.")
         _compare_entities(new_routes, old_routes)
+        log.info("Comparing workflows.")
         _compare_entities(new_workflows, old_workflows)
-
-    def _parse_config_from_file(self):
-        """Parse config fields from yaml file and sort them by name."""
-        log.info("Loading config from file.")
-        with self.config_path.open("r") as config_file:
-            new_config = safe_load(config_file)
-
-        # okay to crash here if the configuration is invalid
-        raw_config = RawConfig.model_validate(new_config)
-
-        # Validator should take care of None names, so all should be populated
-        routes = sorted(raw_config.routes, key=lambda route: route.name)  # type: ignore
-        workflows = sorted(raw_config.workflows, key=lambda workflow: workflow.name)
-
-        log.debug("Deserializing model SchemaPack information.")
-        models = []
-        for raw_model in sorted(raw_config.models, key=lambda model: model.name):
-            # Convert config model with serialized schema to internal representation using
-            # an actual schemapack object, where applicable
-            schemapack = None
-            if raw_model.schema_:
-                try:
-                    schemapack = SchemaPack.model_validate(raw_model.schema_)
-                except ValidationError:
-                    log.error(
-                        "Could not parse SchemaPack information for %s.", raw_model.name
-                    )
-                    break
-            model = InternalModel(
-                **raw_model.model_dump(exclude={"schema_"}), schema_=schemapack
-            )
-            models.append(model)
-
-        return models, routes, workflows
 
     async def _get_persisted_config(self):
         """Fetch config fields from persistence layer and sort them by name."""
+        log.info("Loading old config from persistence layer.")
         persisted_models = []
         async for persisted_model in self.model_dao.find_all(mapping={}):
             # Convert DTO model with serialized schema to internal representation using
@@ -160,6 +145,42 @@ class ConfigManager(ConfigManagerPort):
 
         return models, routes, workflows
 
+    def _parse_config_from_file(self):
+        """Parse config fields from yaml file and sort them by name."""
+        log.info("Loading new config from file.")
+        with self.config_path.open("r") as config_file:
+            new_config = safe_load(config_file)
+
+        # okay to crash here if the configuration is invalid
+        raw_config = RawConfig.model_validate(new_config)
+
+        # Validator should take care of None names, so all should be populated
+        routes = sorted(raw_config.routes, key=lambda route: route.name)  # type: ignore
+        workflows = sorted(raw_config.workflows, key=lambda workflow: workflow.name)
+
+        log.info("Deserializing model SchemaPack information.")
+        models = []
+        for raw_model in sorted(raw_config.models, key=lambda model: model.name):
+            # Convert config model with serialized schema to internal representation using
+            # an actual schemapack object, where applicable
+            schemapack = None
+            if raw_model.schema_:
+                try:
+                    schemapack = SchemaPack.model_validate(raw_model.schema_)
+                except ValidationError:
+                    log.error(
+                        "Could not parse SchemaPack information for %s. Continuing with existing, persisted data instead.",
+                        raw_model.name,
+                    )
+                    self.use_persisted_config = True
+                    break
+            model = InternalModel(
+                **raw_model.model_dump(exclude={"schema_"}), schema_=schemapack
+            )
+            models.append(model)
+
+        return models, routes, workflows
+
 
 def _compare_entities[ConfigField: Route | RawRoute | Workflow](
     new: list[ConfigField], old: list[ConfigField]
@@ -172,7 +193,7 @@ def _compare_entities[ConfigField: Route | RawRoute | Workflow](
         raise ComparisonMismatchError("Different amount of config entities.")
     for n, o in zip(new, old, strict=True):
         if n != o:
-            raise ComparisonMismatchError("Mismatching config entity.")
+            raise ComparisonMismatchError(f"Mismatching config entity: {n.name}.")
 
 
 def _compare_models(new: list[RawModel], old: list[PersistedModel]):
@@ -204,6 +225,6 @@ def _compare_models(new: list[RawModel], old: list[PersistedModel]):
                     f"Mismatching fields on EMIM model {new_model.name}."
                 )
         elif new_model.schema_:
-            raise ComparisonMismatchError(
+            raise ValueError(
                 f"SchemaPack provided for non EMIM model {new_model.name}."
             )
