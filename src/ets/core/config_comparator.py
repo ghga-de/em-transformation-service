87faf89623171a9cf617012d16ce275c553eefa6
Bilge Sürün <bilge.sueruen@uni-tuebingen.de>
2026-03-02 13:54:05 +0100

    Update src/ets/core/config_comparator.py
    Co-authored-by: Thomas Zajac <thomas-jakob.zajac@uni-tuebingen.de>

--- src/ets/core/config_comparator.py
+++ src/ets/core/config_comparator.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Contains functionality to compare transformation config."""
+"""Contains functionality to compare transformation configs."""
 
 import logging
 
